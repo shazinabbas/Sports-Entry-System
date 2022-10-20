6f8be27760e4108a4941d36e7ab02db4201defd0
Kshitij Singh <91209593+mekshitijsingh@users.noreply.github.com>
2022-10-21 00:15:45 +0530

    Update Investigatory Project.cpp

--- Investigatory Project.cpp
+++ Investigatory Project.cpp
@@ -1600,7 +1600,25 @@ while (myfile.good())
     cout << endl;
 
 }
-"
+void palin(num) {
+	int num, rev=0, rem, temp;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    temp = num;
+    while(temp>0)
+    {
+        rem = temp%10;
+        rev = (rev*10)+rem;
+        temp = temp/10;
+    }
+    if(rev==num)
+        cout<<"\nIt is a Palindrome Number";
+    else
+        cout<<"\nIt is not a Palindrome Number";
+    cout<<endl;
+    return 0;
+}
+
 void user::display()
 {
     int div_choice;
