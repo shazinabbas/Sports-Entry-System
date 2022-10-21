7c4ba255216d8efae2b7accbf391d1b8b2f82912
Kshitij Singh <91209593+mekshitijsingh@users.noreply.github.com>
2022-10-21 12:52:31 +0530

    Update Investigatory Project.cpp

--- Investigatory Project.cpp
+++ Investigatory Project.cpp
@@ -1591,7 +1591,28 @@ void palin(num) {
     cout<<endl;
     return 0;
 }
-
+//kshitij
+//Linked list
+struct Node {
+   int data;
+   struct Node *next;
+};
+struct Node* head = NULL;
+void insert(int new_data) {
+   struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+   new_node->data = new_data;
+   new_node->next = head;
+   head = new_node;
+}
+void display() {
+   struct Node* ptr;
+   ptr = head;
+   while (ptr != NULL) {
+      cout<< ptr->data <<" ";
+      ptr = ptr->next;
+   }
+}
+//End
 void user::display()
 {
     int div_choice;
